cece06892ebc4aef8640dfa8acc13e88f0b139a1
vaibhav singh <vaibhav.s@iiiconsulting.in>
2018-01-30 17:49:12 +0530

    New favourites group created

--- White Labelling Demo/Favourites/Controllers/FavouritesViewController.swift
+++ White Labelling Demo/Favourites/Controllers/FavouritesViewController.swift
@@ -12,8 +12,7 @@ class FavouritesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
 }
