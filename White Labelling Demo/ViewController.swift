0d17546097eb74295859e4bf518b695936751910
vaibhav singh <vaibhavs.1307@gmail.com>
2018-01-30 13:16:15 +0530

    Initial Commit

---
+++ White Labelling Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  White Labelling Demo
+//
+//  Created by vaibhav singh on 30/01/18.
+//  Copyright © 2018 vabappd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
