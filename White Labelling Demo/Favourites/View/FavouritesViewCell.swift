268d00dcf724eaf57cbb7bed4969685a57c060a9
vaibhav singh <vaibhav.s@iiiconsulting.in>
2018-02-01 15:12:50 +0530

    Favourites Controller colour setup complete

--- White Labelling Demo/Favourites/View/FavouritesViewCell.swift
+++ White Labelling Demo/Favourites/View/FavouritesViewCell.swift
@@ -11,9 +11,7 @@ import UIKit
 class FavouritesViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .yellow
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
